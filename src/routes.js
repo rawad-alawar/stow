c5f9f2d9dd804530c06076f47e23102b15bf65f2
oliver-jk-redding <oliver.jk.redding@gmail.com>
2016-05-06 10:35:54 +1200

    add example routes

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,12 @@ import React from 'react'
 import {Router, Route, browserHistory} from 'react-router'
 
 import Home from './components/Home'
+import PageTwo from './components/PageTwo'
 
 export default (
   <Router history={browserHistory}>
-    <Route path='/' component={Home} />
+    <Route path='/' component={Home}>
+      <Route path='/pageTwo' component={PageTwo}/>
+    </Route>
   </Router>
 )
