d72b2e59f1ab922c7300babc0a6b09f52cd6c798
Keakone <keanucarnevale@gmail.com>
2016-05-06 19:26:49 +1200

    commit before leaving

--- src/routes.js
+++ src/routes.js
@@ -2,23 +2,23 @@ import React from 'react'
 import {Router, Route, hashHistory, IndexRoute} from 'react-router'
 
 import Home from './components/Home'
-import Login from './components/Login'
 import Signup from './components/Signup'
 import Upload from './components/Upload'
 import Jumbotron from './components/Jumbotron'
 import Dashboard from './components/Dashboard'
 import Listing from './components/Listing'
+import Login from './components/Login'
+
 
 
 export default (
   <Router history={hashHistory}>
     <Route path='/' component={Home}>
     	<IndexRoute component={Dashboard}/>
-      <Route path='login' component={login}/>
+      <Route path='login' component={Login}/>
       <Route path='signup' component={Signup}/>
       <Route path='upload' component={Upload}/>
       <Route path='listing' component={Listing}/>
-
     </Route>
   </Router>
 )
