e47f1b89db3d043f853749d926a0a64e5ce0fd26
Rawad A-lawar <email": null>
2016-05-06 14:21:38 +1200

    add listing function

---
+++ src/models/testSignup.js
@@ -0,0 +1,14 @@
+var saveUserSignup = require('./saveUserSignup')
+var test = require('tape')
+
+data = { firstname: 'Mahuta',
+  lastname: 'Hoerara-Smith',
+  username: 'DEATHGUN',
+  email: 'deathgun@riot.com',
+  password: 'ilovemum' }
+
+test('sign up test', function(t){
+  t.equal(typeof saveUserSignup, 'function')
+  t.equal(saveUserSignup(data), data)
+  t.end()
+})
