70900fbe55a81dcd8cef2e0cace049b2cd031267
Keakone <keanucarnevale@gmail.com>
2016-05-07 17:50:48 +1200

    listings appending to homepage

--- src/components/ListingThumbnail.js
+++ src/components/ListingThumbnail.js
@@ -4,13 +4,13 @@ import {Link} from 'react-router'
 
 class ListingThumbnail extends Component {
   render() {
-      console.log('props',this.props)
+      console.log('props',this.props.listing)
 
     return (
         <div className="col-sm-3">
-          <img className="img" src={this.props.url} alt="this is the image" width="200" height="140"/>
-          <h2>{this.props.price}</h2>
-          <p>{this.props.details}</p>
+          <img className="img" src={this.props.listing.get('url')} alt="this is the image" width="200" height="140"/>
+          <h2>${this.props.listing.get('price')}</h2>
+          <p>{this.props.listing.get('description')}</p>
           <p><a className="btn btn-default" href="#" role="button">View stow &raquo;</a></p>
         </div>
     )
