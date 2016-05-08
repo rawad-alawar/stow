a1765c045bf2b6c7e4b9455285feaf3c2f23b31b
oliver-jk-redding <oliver.jk.redding@gmail.com>
2016-05-08 17:08:46 +1200

    deal with errors

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,6 +4,7 @@ import {hashHistory, Link} from 'react-router'
 import request from 'superagent'
 
 import {loginOrSignUp} from './utils'
+import ErrorCase from './Error'
 
 class Signup extends Component {
 
@@ -18,7 +19,12 @@ class Signup extends Component {
       lastName: this.refs.lastName.value
     }
 
-    loginOrSignUp('/signup', formData, this.props.setCurrentUser)
+    loginOrSignUp('/signup', formData, this.props.setCurrentUser, this.props.dealWithError)
+  }
+
+  printErrorMsg() {
+    if(this.props.error.length > 0)
+      return <ErrorCase error={this.props.error}/>
   }
 
   render() {
@@ -26,15 +32,11 @@ class Signup extends Component {
       <div className="jumbotron col-centered col-sm-4 text-center">
         <form className="form-signup">
           <h2 className="form-signup-heading">Create your account today</h2>
-          <label className="sr-only">Desired Username</label>
           <input type="text" id="inputUsername" className="form-control" placeholder="Your username" ref="username" required autofocus/>
-          <label for="inputPassword" className="sr-only">Password</label>
+          {this.printErrorMsg()}
           <input type="password" id="inputPassword" className="form-control" placeholder="Password" ref="password" required/>
-          <label className="sr-only">E-mail</label>
           <input type="email" id="inputEmail" className="form-control" placeholder="Your email" ref="email" required autofocus/>
-          <label className="sr-only">First-name</label>
           <input type="text" id="firstName" className="form-control" placeholder="Your name" ref="firstName" required autofocus/>
-          <label className="sr-only">Last-name</label>
           <input type="text" id="lastName" className="form-control" placeholder="Your last name" ref="lastName" required autofocus/>
           <button type="button" className="btn btn-lg btn-primary" onClick={this.handleSubmit.bind(this)}>Sign up!</button>
         </form>
@@ -44,7 +46,9 @@ class Signup extends Component {
 }
 
 function mapStateToProps(state) {
-  return {}
+  return {
+    error: state.get('errorMsg')
+  }
 }
 
 function mapDispatchToProps(dispatch) {
@@ -53,6 +57,12 @@ function mapDispatchToProps(dispatch) {
       dispatch({
         type: 'SET_CURRENT_USER',
         user: user
+      })
+    },
+    dealWithError: errorMsg => {
+      dispatch({
+        type: 'SAVE_ERROR_TO_PROPS',
+        errorMsg: errorMsg
       })
     }
   }
