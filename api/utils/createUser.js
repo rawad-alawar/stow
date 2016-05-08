c4808cc4812b8306cef13326e8f58fd708a6e34a
George Armstrong <email": null>
2016-05-08 16:14:48 +1200

    .

--- api/utils/createUser.js
+++ api/utils/createUser.js
@@ -9,10 +9,6 @@ function createUser(data, hash){
     password: hash,
     created_at: Date()
   })
-<<<<<<< HEAD:src/utils/createUser.js
-
-=======
->>>>>>> 2d7511b1c9673f82445b51da66cf24b3118b5f50:api/utils/createUser.js
 }
 
 module.exports = createUser
