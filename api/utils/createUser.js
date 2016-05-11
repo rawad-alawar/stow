df200b790bc5f0ce2429001e00e3a03e71bae3f8
oliver-jk-redding <oliver.jk.redding@gmail.com>
2016-05-11 19:28:19 +1200

    fix knex returning error in createuser

--- api/utils/createUser.js
+++ api/utils/createUser.js
@@ -1,7 +1,7 @@
 var knex = require('./connection')
 
 function createUser(data, hash){
-  return knex('users').insert({
+  return knex('users').returning('user_ID').insert({
     username: data.username.value,
     password: hash,
     email: data.email.value
